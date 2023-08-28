2fd2b6918e5968e52ed941a05bb389d68b5db3d1
Shota Kikozashvili <shota.kikozashvili@liveviewtech.com>
2023-08-28 09:23:46 -0600

    RNSP-003 | apollo client config

--- App.js
+++ App.js
@@ -1,12 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: 'https://main--spacex-l4uc6p.apollographos.net/graphql',
+  cache: new InMemoryCache(),
+});
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ApolloProvider client={client}>
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View>
+  </ApolloProvider>
   );
 }
 
