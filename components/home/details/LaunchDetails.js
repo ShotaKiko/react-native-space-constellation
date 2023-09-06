9d4619e14c4de9722f6986c83900e0abc0d9c360
Shota Kikozashvili <shota.kikozashvili@liveviewtech.com>
2023-09-06 08:54:47 -0600

    RNSP-006 | Additional fields on details page

--- components/home/details/LaunchDetails.js
+++ components/home/details/LaunchDetails.js
@@ -5,11 +5,14 @@ import styles from "./LaunchDetails.styles";
 function LaunchDetails({ navigation, props })  {
     return (
       <View style={styles.container}>
+        <Text>{props.mission_name}</Text>
+        <Text>{props.details}</Text>
         <Text>Additional Launch Information</Text>
-        
-        {/* TODO: Details view */}
-        <View style={styles.content}>Details below</View>
-
+        <View style={styles.content}>
+          <Text>Rocket details</Text>
+          <Text>{props.rocket.rocket_name}</Text>
+          <Text>{props.rocket.rocket_type}</Text>
+        </View>
         <Button
             title="Back to Home"
             // TODO: move on press logic to utils file
